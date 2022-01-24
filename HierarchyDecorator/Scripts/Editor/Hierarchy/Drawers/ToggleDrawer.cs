fb6dc1338418b0b7f66a7221f17dc3684ea4843c
Wooshii <wooshiidev@gmail.com>
2022-01-24 04:55:21 +0000

    Implemented basic Activation Swiping

--- HierarchyDecorator/Scripts/Editor/Hierarchy/Drawers/ToggleDrawer.cs
+++ HierarchyDecorator/Scripts/Editor/Hierarchy/Drawers/ToggleDrawer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEditor.Experimental.SceneManagement;
 using UnityEngine;
 
@@ -6,45 +7,94 @@ namespace HierarchyDecorator
 {
     public class ToggleDrawer : HierarchyDrawer
     {
-        protected override void DrawInternal(Rect rect, GameObject instance, Settings _settings)
+        private bool isHolding = false;
+
+        private Event CurrentEvent => Event.current;
+        private List<Object> swipedInstances = new List<Object> ();
+
+        protected override void DrawInternal(Rect rect, GameObject instance, Settings settings)
         {
             rect.width = 16f;
 
 #if UNITY_2019_1_OR_NEWER
             rect.x = 32;
+#else
+            rect.x = 1f;
+            rect.y--;
+#endif
 
-            bool isActive = instance.activeInHierarchy;
+            int button = CurrentEvent.button;
+            EventType eventType = CurrentEvent.type;
+
+            if (!isHolding)
+            {
+                isHolding = eventType == EventType.MouseDown && button == 0;
+            }
+            else
+            if (eventType == EventType.MouseUp)
+            {
+                isHolding = false;
+                swipedInstances.Clear ();
+            }
+
+            if (isHolding)
+            {
+                HandleSwiping (rect, instance);
+            }
+
+            DrawToggles (rect, instance);
+        }
+
+        protected override bool DrawerIsEnabled(Settings settings, GameObject instance)
+        {
+            return settings.globalData.showActiveToggles && PrefabStageUtility.GetCurrentPrefabStage () == null;
+        }
+
+        private void HandleSwiping(Rect rect, GameObject instance)
+        {
+            int instanceID = instance.GetInstanceID ();
+
+            if (swipedInstances.Contains(instance))
+            {
+                return;
+            }
+
+            if (rect.Contains(CurrentEvent.mousePosition))
+            {
+                Undo.RecordObject (instance, string.Format ("Changed the active state of an {0}", instance.name));
+
+                swipedInstances.Add (instance);
+                instance.SetActive (!instance.activeSelf);
+            }
+        }
+
+        private void DrawToggles(Rect rect, GameObject instance)
+        {
+            bool isActive = instance.activeSelf;
+
+#if UNITY_2019_1_OR_NEWER
             GUIStyle toggleStyle = isActive ? Style.Toggle : Style.ToggleMixed;
 
             EditorGUI.BeginChangeCheck ();
             {
-                isActive = EditorGUI.Toggle (rect, instance.activeSelf, toggleStyle);
+                //isActive = 
+                    EditorGUI.Toggle (rect, isActive, toggleStyle);
             }
             if (EditorGUI.EndChangeCheck ())
             {
-                Undo.RecordObject (instance, string.Format("Changed the active state of an {0}", instance.name));
+                Undo.RecordObject (instance, string.Format ("Changed the active state of an {0}", instance.name));
                 instance.SetActive (isActive);
             }
 #else
-            rect.x = 1f;
-            rect.y--;
-
-            bool isActive = instance.activeInHierarchy;
-
             EditorGUI.BeginChangeCheck ();
             {
-                isActive = EditorGUI.Toggle (rect, instance.activeSelf);
+                isActive = EditorGUI.Toggle (rect, isActive);
             }
             if (EditorGUI.EndChangeCheck ())
             {
                 instance.SetActive (isActive);
             }
 #endif
-        }
-
-        protected override bool DrawerIsEnabled(Settings _settings, GameObject instace)
-        {
-           return _settings.globalData.showActiveToggles && PrefabStageUtility.GetCurrentPrefabStage() == null;
         }
     }
 }
