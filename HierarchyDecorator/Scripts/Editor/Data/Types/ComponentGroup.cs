b68b40764b91be09107610cdb4594ef520b09e02
Wooshii <wooshiidev@gmail.com>
2022-09-10 09:56:16 +0100

    Added return type for Add/Remove methods in Component Group & Commented Class

--- HierarchyDecorator/Scripts/Editor/Data/Types/ComponentGroup.cs
+++ HierarchyDecorator/Scripts/Editor/Data/Types/ComponentGroup.cs
@@ -4,17 +4,57 @@ using System.Collections.Generic;
 
 namespace HierarchyDecorator
 {
+    /// <summary>
+    /// Interface for a named group of elements of type <typeparamref name="T"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGroup<T> where T : IComparable<T>
+    {
+        /// <summary>
+        /// The name of the group.
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// The number of elements apart of this group.
+        /// </summary>
+        int Count { get; }
+
+        /// <summary>
+        /// Get an instance in this group.
+        /// </summary>
+        /// <param name="index">The element index.</param>
+        /// <returns>The found element.</returns>
+        T Get(int index);
+
+        /// <summary>
+        /// Add an instance to this group.
+        /// </summary>
+        /// <param name="instance">The instance to add.</param>
+        /// <returns>Returns a bool based on if the instance was added to the group successfully.</returns>
+        bool Add(T instance);
+
+        /// <summary>
+        /// Remove an instance that belongs to this group.
+        /// </summary>
+        /// <param name="instance">The instance to remove.</param>
+        /// <returns>Returns a bool based on if the instance was removed from the group suggessfully.</returns>
+        bool Remove(T instance);
+    }
+
     [Serializable]
     /// <summary>
     /// Collection of components for icons
     /// </summary>
     /// <typeparam name="T"></typeparam>
-    public class ComponentGroup 
+    public class ComponentGroup : IGroup<ComponentType>
     {
         // Fields
 
         [SerializeField] protected string name;
         [SerializeField] protected List<ComponentType> components = new List<ComponentType>();
+
+        private HashSet<ComponentType> a;
 
         // Properties
 
@@ -26,6 +66,11 @@ namespace HierarchyDecorator
             get
             {
                 return name;
+            }
+
+            set
+            {
+                name = value;
             }
         }
 
@@ -64,10 +109,55 @@ namespace HierarchyDecorator
         /// Add an element to the group.
         /// </summary>
         /// <param name="component">The component to add to the group.</param>
-        public void Add(ComponentType component)
+        /// <returns>Returns a bool based on if the instance was added to the group successfully.</returns>
+        public bool Add(ComponentType component)
         {
+            if (component == null)
+            {
+                Debug.LogError("Attempted to add null component to a group.");
+                return false;
+            }
+
             components.Add(component);
+            return true;
         }
+
+        /// <summary>
+        /// Remove an element from the group.
+        /// </summary>
+        /// <param name="component">The component to remove.</param>
+        /// <returns>Returns a bool based on if the instance was remove from the group successfully.</returns>
+        public bool Remove(ComponentType component)
+        {
+            // Cannot remove a null component, even if null components exist, it's not explict enough.
+
+            if (component == null)
+            {
+                Debug.LogError("Attempted to remove null component from a group.");
+                return false;
+            }
+
+            if (!components.Remove(component))
+            {
+                Debug.LogError("Cannot remove a component that does not belong to the group.");
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool Remove(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                Debug.LogError("Given index to remove component is out of range.");
+                return false;
+            }
+
+            components.RemoveAt(index);
+            return true;
+        }
+
 
         /// <summary>
         /// Get a component element in the group.
@@ -78,13 +168,16 @@ namespace HierarchyDecorator
         {
             if (index < 0 || index >= components.Count)
             {
-                //throw new IndexOutOfRangeException();
+                Debug.LogError("Index is out of range of group size.");
                 return null;
             }
 
             return components[index];
         }
 
+        /// <summary>
+        /// Sort the group.
+        /// </summary>
         public void Sort()
         {
             components.Sort();
@@ -92,21 +185,36 @@ namespace HierarchyDecorator
 
         // Queries
 
-        public bool TryGetComponent(Type type, out ComponentType foundComponent)
+        /// <summary>
+        /// Attempt to look for a component that's been added to this group.
+        /// </summary>
+        /// <param name="type">The type of component to look for.</param>
+        /// <param name="component">The found component if any are valid. Will return null if no component is found.</param>
+        /// <returns>Returns a bool based on if a valid component is found or not.</returns>
+        public bool TryGetComponent(Type type, out ComponentType component)
         {
             for (int i = 0; i < Count; i++)
             {
-                ComponentType component = components[i];
+                component = components[i];
 
                 if (component.Type == type)
                 {
-                    foundComponent = component;
                     return true;
                 }
             }
 
-            foundComponent = null;
+            component = null;
             return false;
+        }
+
+        /// <summary>
+        /// Check if a component is in this group.
+        /// </summary>
+        /// <param name="component">The component to check.</param>
+        /// <returns>Returns true if the given component is found in the group otherwise it will return false.</returns>
+        public bool Contains(ComponentType component)
+        {
+            return components.Contains(component);
         }
 
         // Indexer
