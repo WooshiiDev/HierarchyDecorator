b71707807f3eb653156dc843e11e88c921d83046
Wooshii <wooshiidev@gmail.com>
2023-01-28 19:55:33 +0000

    Fixed invalid parameter error apparently missed?

--- HierarchyDecorator/Scripts/Editor/Tabs/IconTab.cs
+++ HierarchyDecorator/Scripts/Editor/Tabs/IconTab.cs
@@ -633,7 +633,7 @@ namespace HierarchyDecorator
                         break;
 
                     case 3: // Other options
-                        ShowCustomGroupMenu(index, group);
+                        ShowCustomGroupMenu(index);
                         break;
                 }
             }
