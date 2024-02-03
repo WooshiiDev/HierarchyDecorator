a4ba380092d9a63e791bd7d27f40bb8aa289b028
Damian Slocombe <wooshiidev@gmail.com>
2024-02-03 20:10:05 +0000

    Implemented: Tag drop down with selection to modify instance

--- HierarchyDecorator/Scripts/Editor/Hierarchy/Info/TagInfo.cs
+++ HierarchyDecorator/Scripts/Editor/Hierarchy/Info/TagInfo.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEditor;
 using UnityEngine;
+using UnityEditorInternal;
 
 namespace HierarchyDecorator
 {
@@ -12,7 +13,49 @@ namespace HierarchyDecorator
                 return;
             }
 
-            instance.tag = EditorGUI.TagField(rect, instance.tag, Style.SmallDropdown);
+            EditorGUI.LabelField(rect, instance.tag, Style.SmallDropdown);
+
+            if (settings.globalData.clickToSelectTags)
+            {
+                Event e = Event.current;
+                bool hasClicked = rect.Contains(e.mousePosition) && e.type == EventType.MouseDown;
+
+                if (!hasClicked)
+                {
+                    return;
+                }
+
+                GameObject[] selection = Selection.gameObjects;
+
+                if (selection.Length < 2)
+                {
+                    Selection.SetActiveObjectWithContext(instance, null);
+                }
+
+                GenericMenu menu = new GenericMenu();
+                bool setChildLayers = settings.globalData.applyChildLayers;
+
+                foreach (string tag in InternalEditorUtility.tags)
+                {
+                    menu.AddItem(new GUIContent(tag), false, () =>
+                    {
+                        Undo.RecordObjects(Selection.gameObjects, "Tag Updated");
+
+                        foreach (GameObject go in Selection.gameObjects)
+                        {
+                            go.tag = tag;
+
+                            if (Selection.gameObjects.Length == 1)
+                            {
+                                Selection.SetActiveObjectWithContext(null, null);
+                            }
+                        }
+                    });
+                }
+
+                menu.ShowAsContext();
+                e.Use();
+            }
         }
 
         protected override int GetGridCount()
