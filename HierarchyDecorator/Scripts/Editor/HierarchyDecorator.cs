1813d9022fce6fe09c88aba40a8aecea23807f1e
Damian Slocombe <wooshiidev@gmail.com>
2024-02-04 20:20:57 +0000

    Comment clean up

--- HierarchyDecorator/Scripts/Editor/HierarchyDecorator.cs
+++ HierarchyDecorator/Scripts/Editor/HierarchyDecorator.cs
@@ -32,9 +32,6 @@ namespace HierarchyDecorator
 
         private static HierarchyInfo[] Info = new HierarchyInfo[]    
         {
-            //new TagInfo(),
-            //new LayerInfo(),
-
             new TagLayerInfo(),
             new ComponentIconInfo()
         };
