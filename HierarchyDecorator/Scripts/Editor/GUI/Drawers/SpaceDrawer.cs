f4f1199c6cbe13ca2e049aa4da4a8aae5d671348
Damian Slocombe <wooshiidev@gmail.com>
2024-02-04 20:19:58 +0000

    Drawers can now have spaces between elements

---
+++ HierarchyDecorator/Scripts/Editor/GUI/Drawers/SpaceDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace HierarchyDecorator
+{
+    public class SpaceDrawer : IDrawable
+    {
+        private readonly float height;
+
+        public SpaceDrawer(float height)
+        {
+            this.height = height;
+        }
+
+        public void OnDraw()
+        {
+            HierarchyGUI.Space(height);
+        }
+    }
+}
