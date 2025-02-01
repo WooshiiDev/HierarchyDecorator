2bc53cdb464062ad17f1f8105d154f2495becbc8
Wooshii <wooshiidev@gmail.com>
2025-02-01 11:42:47 +0000

    Moved serialized properly apply call to base tab class

--- HierarchyDecorator/Scripts/Editor/Tabs/SettingsTab.cs
+++ HierarchyDecorator/Scripts/Editor/Tabs/SettingsTab.cs
@@ -66,6 +66,7 @@ namespace HierarchyDecorator
             }
             if (EditorGUI.EndChangeCheck())
             {
+                serializedSettings.ApplyModifiedProperties();
                 IsDirty = true;
             }
 
