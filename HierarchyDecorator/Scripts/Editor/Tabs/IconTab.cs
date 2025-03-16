0d7e71efee20c0830dde7f355ca053248e86c92a
Indie Seal <alxoanimations@gmail.com>
2025-03-16 13:31:52 -0700

    Toggable Icons Feature
    What this feature does is let you choose in the settings whether when an icon is clicked, if the component should get toggled or not.

--- HierarchyDecorator/Scripts/Editor/Tabs/IconTab.cs
+++ HierarchyDecorator/Scripts/Editor/Tabs/IconTab.cs
@@ -175,7 +175,7 @@ namespace HierarchyDecorator
             display.ToggleStyle = Style.ToolbarButtonLeft;
 
             CreateDrawableGroup("Settings")
-                .RegisterSerializedProperty(serializedTab, "enableIcons", "stackDuplicateIcons", "showMissingScriptWarning");
+                .RegisterSerializedProperty(serializedTab, "enableIcons", "togglableIcons", "stackDuplicateIcons", "showMissingScriptWarning");
         }
 
         // Methods
